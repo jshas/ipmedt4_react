6b8c26ee44134b41990dc88307d49b379196905c
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-06-06 22:04:41 +0200

    init App.js: browser-route toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    // Verwijderen van de makeApiCall naar SearchBar
+    // State wordt bijgehouden in de 
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/">
+                        <main>
+                          <h1>Dit is een lege pagina.</h1>
+                        </main>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    };
 }
 
 export default App;
