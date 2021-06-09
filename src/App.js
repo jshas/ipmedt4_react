05e30db28d13eb3a0beffca14b03d6ac35ae392a
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-06-09 14:26:42 +0200

    ProductCards worden gerendered met JSON API Data nu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useParams,
+// } from "react-router-dom";
 
-import './App.css';
+import axios from "axios";
+
+import ProductCard from "./ProductCard";
+import "./App.css";
 
 class App extends React.Component {
+  state = { productList: [] };
 
-    // Verwijderen van de makeApiCall naar SearchBar
-    // State wordt bijgehouden in de 
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/">
-                        <main>
-                          <h1>Dit is een lege pagina.</h1>
-                        </main>
-                    </Route>
-                </Switch>
-            </Router>
-        );
-    };
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const BASE_URL = "http://127.0.0.1:8000/api/products/";
+    axios
+      .get(BASE_URL)
+      .then((res) => {
+        this.setState({ productList: res.data });
+        })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  selectProduct(id) {
+    return this.state.productList;
+  }
+
+  products = [];
+  
+  render() {
+    this.products = this.state.productList.map((product) => {
+      return (
+      <li key={product.id.toString()}>
+        <ProductCard key={product.key} value={product} />
+      </li>
+      )
+    });
+    return(
+      <ul className="productGrid">
+        {this.products}
+      </ul>
+    )
+};
 }
-
 export default App;
+
---
+++ src/ProductCard.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState, state} from 'react';
+import axios from 'axios';
+
+// import OrderButton from './OrderButton';
+
+import './ProductCard.css';
+
+
+const ProductCard = (props) => {
+
+    const localHost = 'http://localhost:8000/' 
+    
+    // useEffect(()=>{
+    //     this.onClick{
+
+    //     }
+    // })
+
+    function handleOrder(){
+        
+
+    };
+
+    return (
+        //<li>
+            <article className="productCard" >
+                <section className="productCard__label">
+                    <h2 className="productCard__category">{props.value.category || "Categorie"}</h2>
+                    <p class="productCard__subCategory">{props.value.sub_category}</p>
+                </section>
+                <header className="productCard__header">
+                    <h2 className="productCard__heading">{props.value.brand || "Brand"} {props.value.model || "Model"}</h2>
+                </header>
+                <section className="ProductCard__body">
+                    <figure className="productCard__figure"><img className="productCard__img" src={localHost + props.value.img_path} alt={props.value.img_alt} /></figure>
+                    <p className="productCard__description">{props.value.description}</p>
+                </section>
+                <button className="productCard__button" type="button" onClick={handleOrder}> Voeg toe aan bestelling </button>
+            </article>
+        //</li>
+        );
+}
+
+export default ProductCard;
