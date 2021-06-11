1cc17e18191e9cb9c663b169da682d419c4eb648
dkalemli <dogukankalemli@hotmail.com>
2021-06-11 20:00:26 +0200

    Basis login pagina

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Login.css";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <article className="loginSection">
+                <section className="loginSection__figure"></section>
+                <section className="loginSection__form">
+                    
+                </section>
+            </article>
+        );
+    }
+}
+
+export default Login;
