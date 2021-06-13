12cfbe1ffb21b5da546915d76df95696284c90b1
dkalemli <dogukankalemli@hotmail.com>
2021-06-14 00:28:46 +0200

    sanctum geïntegreerd met loginpagina

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,7 @@
 /* External imports */
-import React from "react";
+import React, { useState } from "react";
+import api from "../util/api";
+import { logIn } from "../util/auth";
 
 /* Internal imports */
 import Logo from "../img/jungheinrich_logo.png";
@@ -7,44 +9,77 @@ import Logo from "../img/jungheinrich_logo.png";
 /* CSS imports */
 import "./LoginPage.css";
 
-class Login extends React.Component {
-    render() {
-        return (
-            <article className="loginSection">
-                <section className="loginSection__figure"></section>
-                <section className="loginSection__form">
-                    <picture className="loginSection__form__picture">
-                        <img src={Logo} alt="Jungheinrich logo" />
-                    </picture>
+const Login = () => {
+    const [formInput, setFormInput] = useState({ email: "", password: "" });
 
-                    <h2 className="loginSection__form__heading">
-                        Member login
-                    </h2>
+    const updateFormInput = (event) => {
+        event.persist();
 
-                    <form className="loginSection__form__field" action="/">
-                        <fieldset className="loginSection__form__inputField">
-                            <label for="email">E-mailadres</label>
-                            <input type="text" id="email" name="email" />
+        setFormInput((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+        }));
+    };
 
-                            <label for="password">Wachtwoord</label>
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                            />
-                        </fieldset>
-                        <fieldset className="loginSection__form__btnField">
-                            <button type="submit">Inloggen</button>
-                        </fieldset>
-                    </form>
+    const signIn = (event) => {
+        event.preventDefault();
 
-                    <p className="loginSection__form__paragraph">
-                        Wachtwoord vergeten?
-                    </p>
-                </section>
-            </article>
-        );
-    }
-}
+        api()
+            .get("/sanctum/csrf-cookie")
+            .then(() => {
+                api()
+                    .post("/login", formInput)
+                    .then((response) => {
+                        if (response.data.error) {
+                            console.log(response.data.error);
+                        } else {
+                            logIn();
+                        }
+                    });
+            });
+    };
+
+    return (
+        <article className="loginSection">
+            <section className="loginSection__figure"></section>
+            <section className="loginSection__form">
+                <picture className="loginSection__form__picture">
+                    <img src={Logo} alt="Jungheinrich logo" />
+                </picture>
+
+                <h2 className="loginSection__form__heading">Member login</h2>
+
+                <form className="loginSection__form__field" action="#">
+                    <fieldset className="loginSection__form__inputField">
+                        <label htmlFor="email">E-mailadres</label>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            onChange={updateFormInput}
+                        />
+
+                        <label htmlFor="password">Wachtwoord</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={updateFormInput}
+                        />
+                    </fieldset>
+                    <fieldset className="loginSection__form__btnField">
+                        <button type="submit" onClick={signIn}>
+                            Inloggen
+                        </button>
+                    </fieldset>
+                </form>
+
+                <p className="loginSection__form__paragraph">
+                    Wachtwoord vergeten?
+                </p>
+            </section>
+        </article>
+    );
+};
 
 export default Login;
---
+++ src/util/auth.js
@@ -0,0 +1,36 @@
+/* External imports */
+import Cookies from "js-cookie";
+import cookie from "cookie";
+
+export const isLoggedIn = (reqCookies = null) => {
+    // if we don't have request cookies, get the cookie from client
+    if (!reqCookies) {
+        return !!Cookies.get("kva_is_user_logged_in");
+    }
+
+    // otherwise get cookie from server
+    return !!cookie.parse(reqCookies).kva_is_user_logged_in;
+};
+
+export const logIn = () => {
+    Cookies.set("kva_is_user_logged_in", true, {
+        expires: 86400,
+        sameSite: "lax",
+    });
+
+    window.location.replace("/");
+};
+
+export const logOut = () => {
+    if (typeof window !== "undefined") {
+        // remove logged in user's cookie and redirect to login page
+        Cookies.remove("kva_is_user_logged_in", {
+            expires: 86400,
+            sameSite: "lax",
+        });
+
+        console.log("Uitgelogd!");
+
+        window.location.replace("/login");
+    }
+};
