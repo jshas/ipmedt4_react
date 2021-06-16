a5c25a79b5875c912a35c84f8ea329bba574e461
dkalemli <dogukankalemli@hotmail.com>
2021-06-16 12:14:35 +0200

    api fix

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -3,108 +3,104 @@ import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
 /* Internal Imports*/
+import api from "../util/api";
 import ProductCard from "../components/layout/ProductCard";
-import "./ProductPage.css";
 
 /* CSS Imports*/
 import "./ProductPage.css";
 
-
 const ProductPage = (props) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [productsList, setProductsList] = useState([]);
-  const [cartList, setCartList] = useState([]);
-  
+    const [isLoading, setIsLoading] = useState(true);
+    const [productsList, setProductsList] = useState([]);
+    const [cartList, setCartList] = useState([]);
 
-  /* CART FUNCTIONS */
-  const checkCartStatus = () => {
-    if (isLoading === false) {
-    }
-  };
+    /* CART FUNCTIONS */
+    const checkCartStatus = () => {
+        if (isLoading === false) {
+        }
+    };
 
-  const addProductToCart = (productId) => {
-    // console.log([...cartList, productId]);
-    if(cartList === []){
-      setCartList(productId);
-    }
-    setCartList(cartList => [...cartList, productId]);
+    const addProductToCart = (productId) => {
+        // console.log([...cartList, productId]);
+        if (cartList === []) {
+            setCartList(productId);
+        }
+        setCartList((cartList) => [...cartList, productId]);
+    };
 
-  };
+    const removeProductFromCart = (productId) => {
+        const updatedList = cartList.filter((id) => id !== productId);
+        setCartList(updatedList);
+    };
 
-  const removeProductFromCart = (productId) => {
-    const updatedList = cartList.filter(id=> id  !== productId);
-    setCartList(updatedList);
-  }
+    // Checks if the supplied productId should be add or removed
+    const updateCart = (productId, action) => {
+        if (action === "add") {
+            addProductToCart(productId);
+        }
+        if (action === "remove") {
+            removeProductFromCart(productId);
+        }
+    };
 
-  // Checks if the supplied productId should be add or removed
-  const updateCart = (productId, action) => {
-    if (action === 'add'){
-      addProductToCart(productId);
-    }
-    if (action === 'remove'){
-      removeProductFromCart(productId);
-    }
-  }
+    // Fetches the product API once when the component is mounted
+    useEffect(() => {
+        const BASE_URL = "http://localhost:8000/api/products/";
+        api()
+            .get(BASE_URL)
+            .then((res) => {
+                setIsLoading(false);
+                setProductsList(res.data);
+                checkCartStatus();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        // getProducts();
+    }, []);
 
-  // Fetches the product API once when the component is mounted
-  useEffect(() => {
-    const BASE_URL = "http://localhost:8000/api/products/";
-    axios
-      .get(BASE_URL)
-      .then((res) => {
-        setIsLoading(false);
-        setProductsList(res.data);
-        checkCartStatus();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    // getProducts();
-  },[]);
+    // Mapping of fetched API products to list.
+    const products = productsList.map((product) => {
+        return (
+            <li className="u-list-style-none" key={product.id.toString()}>
+                <ProductCard
+                    key={product.key}
+                    product={product}
+                    onChange={(productId, action) =>
+                        updateCart(productId, action)
+                    }
+                />
+            </li>
+        );
+    });
 
-  // Mapping of fetched API products to list.
-  const products = productsList.map((product) => {
+    const cartItems = cartList.map((itemId) => {
+        return (
+            <li className="shoppingCart__row" key={itemId.toString()}>
+                <p className="shoppingCard__index">{itemId}</p>
+                <p>{productsList[itemId - 1].brand}</p>
+                <p>{productsList[itemId - 1].model}</p>
+                <p>{"€" + productsList[itemId - 1].price / 100}</p>
+            </li>
+        );
+    });
+
     return (
-      <li className="u-list-style-none" key={product.id.toString()}>
-        <ProductCard
-          key={product.key}
-          product={product}
-          onChange={(productId, action) => updateCart(productId, action)}
-        />
-      </li>
+        <>
+            <ul className="productGrid">
+                {/* This shoppingcart displays a test for productId's.  */}
+                <li className="shoppingCart">
+                    <section>
+                        <h2>Winkelwagen</h2>
+                        <p>Wie niet wagent, wie niet winkelt.</p>
+                        <div className="u-separator"></div>
+                        <ul>{cartItems}</ul>
+                    </section>
+                </li>
+                {products}
+            </ul>
+        </>
     );
-  });  
-
-  
-  const cartItems = cartList.map((itemId) => {
-    return (
-      <li className="shoppingCart__row" key = {itemId.toString()}>
-        <p className="shoppingCard__index">{itemId}</p>
-        <p>{productsList[itemId-1].brand}</p> 
-        <p>{productsList[itemId-1].model}</p>
-        <p>{'€' + (productsList[itemId-1].price /100)}</p>
-      </li>
-    );
-  }); 
-
-  return (
-    <>
-      <ul className="productGrid">
-        {/* This shoppingcart displays a test for productId's.  */}
-        <li className="shoppingCart">
-          <section>
-            <h2>Winkelwagen</h2>
-            <p>Wie niet wagent, wie niet winkelt.</p>
-            <div className="u-separator"></div>
-              <ul>
-                {cartItems}
-             </ul>
-        </section>
-        </li>
-        {products}
-      </ul>
-    </>
-  );
 };
 
 export default ProductPage;
