fef9a47d458aac97ab34ea1f25a3bfc381e43282
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-06-20 12:30:08 +0200

    ProductPage: Verdere refactor en fixes -> fix van user orders merge in productsList -> shoppingCart klasse toegevoegd voor de  parent <li> in ProductPage

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,6 +1,6 @@
 /* External Imports */
 
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import { cloneDeep } from "lodash"; // A function to create deep copies of arrays (and nested levels) to ensure immutable state arrays (e.g.: ProductsList)
 
 /* Internal Imports*/
@@ -12,12 +12,42 @@ import ShoppingCart from "../components/layout/ShoppingCart";
 import "./ProductPage.css";
 
 const ProductPage = (props) => {
-  // State: Flags for content to be displayed
-  const [ordersPlaced, setOrdersPlaced] = useState(false);
-
   // State: Data
   const [productsList, setProductsList] = useState([]);
   const [user, setUser] = useState(-1);
+
+    // GET: Fetches Products from laravel API into the ProductsList state variable
+    useEffect(() => {
+      api()
+        .get("/api/products")
+        .then((res) => {
+          // This maps the inCart value to the productList state
+          let productData = res.data[0].map((product) => {
+            product.inCart = false;
+            return product;
+          });
+          const userOrders = res.data[2];
+          // combinedData is a combination of the products and the user specific orders array.
+          // 
+          let combinedData = productData.map((product)=>{
+            let ordered;
+            if (product.id in Object.keys(userOrders)){
+              ordered = userOrders[product.id];
+              product.ordered = ordered
+              return product;
+            }else{
+              product.ordered = 0;
+              return product;
+            }
+          });
+          setProductsList(productData);
+          setUser(res.data[1]);
+          console.log(combinedData);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
 
   // Sends productId to add or remove function based on supplied action parameter from the ProductCard component
   const updateCart = (productId, action) => {
@@ -28,11 +58,12 @@ const ProductPage = (props) => {
     }
     if (action === "remove") {
       let updateArrayCopy = cloneDeep(productsList);
+      console.log(productId);
       updateArrayCopy[productId - 1].inCart = false;
       setProductsList(updateArrayCopy);
     }
+    // The reset conditional will only trigger when undefined as passed as productId
     if (action === "reset" && productId === undefined) {
-      console.log("reset triggered");
       let resetInCart = productsList.map((product) => {
         product.inCart = false;
         return product;
@@ -41,37 +72,7 @@ const ProductPage = (props) => {
     }
   };
 
-  // GET: Fetches Products from laravel API into the ProductsList state variable
-  useEffect(() => {
-    api()
-      .get("/api/products")
-      .then((res) => {
-        // This maps the inCart value to the productList state
-        let productData = res.data[0].map((e) => {
-          e.inCart = false;
-          return e;
-        });
-        const userOrders = res.data[2];
-        setProductsList(productData);
-        setUser(res.data[1]);
-        console.log(userOrders)
-        let combinedData = productData.map((product)=>{
-          let ordered;
-          if (product.id in Object.keys(userOrders)){
-            ordered = userOrders[product.id];
-            product.ordered = ordered
-            return product;
-          }else{
-            product.ordered = 0;
-            return product;
-          }
-        });
-        console.log(combinedData);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+
 
   // Mapping of fetched API products to list.
   const products = productsList.map((product) => {
@@ -82,6 +83,7 @@ const ProductPage = (props) => {
           product={product}
           updateCart={(productId, action) => updateCart(productId, action)}
           inCart={product.inCart}
+          ordered={product.ordered}
         />
       </li>
     );
@@ -95,11 +97,12 @@ const ProductPage = (props) => {
   return (
     <>
       <ul className="productGrid">
-        <li className="u-list-style-none">
+        <li className="u-list-style-none productGrid__shoppingCart">
           <ShoppingCart
             userId={user}
             cartItems={cartItems}
             resetCart={() => updateCart(undefined, "reset")}
+            removeItem={(productId) => updateCart(productId, 'remove')}
           />
         </li>
         {products}
