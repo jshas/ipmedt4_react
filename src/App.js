6e0f4ac5184b108f4d49825da932d742ef372310
dkalemli <dogukankalemli@hotmail.com>
2021-06-22 19:25:58 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* External imports */
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 /* Internal imports */
@@ -20,65 +20,85 @@ const App = (props) => {
     const filterHandler = (e) => {
         setActiveFilters([...activeFilters, e]);
         console.log(activeFilters);
-    }
+    };
 
+    return (
+        <Switch>
+            <Route path="/" exact>
+                <article className="pageLayout">
+                    <Sidebar
+                        activeFilters={activeFilters}
+                        filterHandler={filterHandler}
+                        className="u-grid-area-sidebar"
+                    />
+                    <ProductPage
+                        className="u-grid-area-content u-list-style"
+                        activeFilters={activeFilters}
+                    />
+                </article>
+            </Route>
 
-        return (
-            <Switch>
-                <Route path="/" exact>
-                    <article className="pageLayout">
-                        <Sidebar activeFilters={activeFilters} filterHandler={filterHandler} className="u-grid-area-sidebar" />
-                        <ProductPage
-                            className="u-grid-area-content u-list-style"
-                            activeFilters={activeFilters}
-                        />
-                    </article>
-                </Route>
+            <Route path="/users" exact>
+                <article className="pageLayout">
+                    <Sidebar
+                        activeFilters={activeFilters}
+                        filterHandler={filterHandler}
+                        className="u-grid-area-sidebar"
+                    />
+                    <UserPage className="Grid" />
+                </article>
+            </Route>
 
-                <Route path="/users" exact>
-                    <article className="pageLayout">
-                        <Sidebar activeFilters={activeFilters} filterHandler={filterHandler} className="u-grid-area-sidebar" />
-                        <UserPage
-                            className="Grid"
-                        />
-                    </article>
-                </Route>
+            <Route path="/users/add">
+                <article className="pageLayout">
+                    <Sidebar
+                        activeFilters={activeFilters}
+                        filterHandler={filterHandler}
+                        className="u-grid-area-sidebar"
+                    />
+                    <AddUsers className="Grid" />
+                </article>
+            </Route>
 
-                <Route path="/users/add">
-                    <article className="pageLayout">
-                        <Sidebar activeFilters={activeFilters} filterHandler={filterHandler} className="u-grid-area-sidebar" />
-                        <AddUsers className="Grid" />
-                    </article>
-                </Route>
+            <Route path="/products" exact>
+                <article className="pageLayout">
+                    <Sidebar
+                        activeFilters={activeFilters}
+                        filterHandler={filterHandler}
+                        className="u-grid-area-sidebar"
+                        style={{ zIndex: 1 }}
+                    />
+                    <ProductPage className="u-grid-area-content u-list-style" />
+                </article>
+            </Route>
 
-                <Route path="/products" exact>
-                    <article className="pageLayout">
-                        <Sidebar activeFilters={activeFilters} filterHandler={filterHandler} className="u-grid-area-sidebar" style={{zIndex:1}}/>
-                        <ProductPage
-                            className="u-grid-area-content u-list-style"
-                        />
-                    </article>
-                </Route>
+            <Route path="/products/export">
+                <article className="pageLayout">
+                    <Sidebar
+                        activeFilters={activeFilters}
+                        filterHandler={filterHandler}
+                        className="u-grid-area-sidebar"
+                    />
+                    <ExportProducts className="Grid" />
+                </article>
+            </Route>
 
-                <Route path="/products/export">
-                    <article className="pageLayout">
-                        <Sidebar activeFilters={activeFilters} filterHandler={filterHandler} className="u-grid-area-sidebar" />
-                        <ExportProducts className="Grid" />
-                    </article>
-                </Route>
+            <Route path="/products/add">
+                <article className="pageLayout">
+                    <Sidebar
+                        activeFilters={activeFilters}
+                        filterHandler={filterHandler}
+                        className="u-grid-area-sidebar"
+                    />
+                    <AddProducts className="Grid" />
+                </article>
+            </Route>
 
-                <Route path="/products/add">
-                    <article className="pageLayout">
-                        <Sidebar activeFilters={activeFilters} filterHandler={filterHandler} className="u-grid-area-sidebar" />
-                        <AddProducts className="Grid" />
-                    </article>
-                </Route>
-
-                <Route path="/login">
-                    <LoginPage />
-                </Route>
-            </Switch>
-        );
-}
+            <Route path="/login">
+                <LoginPage />
+            </Route>
+        </Switch>
+    );
+};
 
 export default App;
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -8,143 +8,172 @@ import api from "../util/api";
 import ProductCard from "../components/layout/ProductCard";
 import ShoppingCart from "../components/layout/ShoppingCart";
 
-
 /* CSS Imports*/
 import "./ProductPage.css";
 
 const ProductPage = (props) => {
-  // State: Data
-  const [productsList, setProductsList] = useState([]);
-  const [user, setUser] = useState(-1);
+    // State: Data
+    const [productsList, setProductsList] = useState([]);
+    const [user, setUser] = useState(-1);
 
-  console.log(props.activeFilters);
+    console.log(props.activeFilters);
 
     // GET: Fetches Products from laravel API into the ProductsList state variable
     useEffect(() => {
-      api()
-        .get("/api/products")
-        .then((res) => {
-          // This maps the inCart value to the productList state
-          let productData = res.data[0].map((product) => {
-            product.inCart = false;
-            return product;
-          });
-          const userOrders = res.data[2];
-          // combinedData is a combination of the products and the user specific orders array.
+        api()
+            .get("/api/products")
+            .then((res) => {
+                // This maps the inCart value to the productList state
+                let productData = res.data[0].map((product) => {
+                    product.inCart = false;
+                    return product;
+                });
+                const userOrders = res.data[2];
+                // combinedData is a combination of the products and the user specific orders array.
 
-          let combinedData = productData.map((product)=>{
-            let ordered;
-            if (product.id in Object.keys(userOrders)){
-              ordered = userOrders[product.id];
-              product.ordered = ordered
-              return product;
-            }else{
-              product.ordered = 0;
-              return product;
-            }
-          });
-          setProductsList(productData);
-          setUser(res.data[1]);
-          console.log(combinedData);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+                let combinedData = productData.map((product) => {
+                    let ordered;
+                    if (product.id in Object.keys(userOrders)) {
+                        ordered = userOrders[product.id];
+                        product.ordered = ordered;
+                        return product;
+                    } else {
+                        product.ordered = 0;
+                        return product;
+                    }
+                });
+                setProductsList(productData);
+                setUser(res.data[1]);
+                console.log(combinedData);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }, []);
 
-  // Sends productId to add or remove function based on supplied action parameter from the ProductCard component
-  const updateCart = (productId, action) => {
-    if (action === "add") {
-      let updateArrayCopy = cloneDeep(productsList);
-      updateArrayCopy[productId - 1].inCart = true;
-      setProductsList(updateArrayCopy);
-    }
-    if (action === "remove") {
-      let updateArrayCopy = cloneDeep(productsList);
-      console.log(productId);
-      updateArrayCopy[productId - 1].inCart = false;
-      setProductsList(updateArrayCopy);
-    }
-    // The reset conditional will only trigger when undefined as passed as productId
-    if (action === "reset" && productId === undefined) {
-      let resetInCart = productsList.map((product) => {
-        product.inCart = false;
-        return product;
-      });
-      setProductsList(resetInCart);
-    }
-  };
-
-  
-  // Mapping of fetched API products to list.
-  const products = productsList.map((product) => {
-    return (
-      <li className="u-list-style-none" key={product.id.toString() } style={{display: "block"}}>
-        <ProductCard
-          key={product.key}
-          product={product}
-          updateCart={(productId, action) => updateCart(productId, action)}
-          inCart={product.inCart}
-          ordered={product.ordered}
-        />
-      </li>
-    );
-  });
-
-
-  // Maps a copy array of productsList with inCart === true for the <ShoppingCart /> component
-  const cartItems = productsList.filter(function (product) {
-    return product.inCart === true;
-  });
-
-  const SearchSort = () => {
-    var searchInput = document.getElementById("searchInput");
-    var filter = searchInput.value.toUpperCase().split(" ");
-    var i;
-    console.log(filter);
-    for(i = 1; i < products.length; i++){
-      var currentProduct = products[i].props.children.props.product;
-      
-      var prodBrand = currentProduct.brand.toUpperCase();
-      var prodCat = currentProduct.category.toUpperCase();
-      var prodDescription = currentProduct.description.toUpperCase();
-      var prodModel = currentProduct.model.toUpperCase();
-      var prodSubCategory = currentProduct.sub_category.toUpperCase();
-
-      var j;
-      document.getElementsByClassName("u-list-style-none")[i].style.display = "none";
-
-      for(j = 0; j < filter.length; j++){
-        if(prodBrand.includes(filter[j]) || prodCat.includes(filter[j]) || prodDescription.includes(filter[j]) ||prodModel.includes(filter[j]) || prodSubCategory.includes(filter[j])){
-          document.getElementsByClassName("u-list-style-none")[i].style.display = "block";
-          var k;
-          for(k = 0; k < filter.length; k++){
-            if(!(prodBrand.includes(filter[k]) || prodCat.includes(filter[k]) || prodDescription.includes(filter[k]) ||prodModel.includes(filter[k]) || prodSubCategory.includes(filter[k]))){
-              document.getElementsByClassName("u-list-style-none")[i].style.display = "none";
-              
-            }
-          }
+    // Sends productId to add or remove function based on supplied action parameter from the ProductCard component
+    const updateCart = (productId, action) => {
+        if (action === "add") {
+            let updateArrayCopy = cloneDeep(productsList);
+            updateArrayCopy[productId - 1].inCart = true;
+            setProductsList(updateArrayCopy);
         }
-      }
-    }
-  }
+        if (action === "remove") {
+            let updateArrayCopy = cloneDeep(productsList);
+            console.log(productId);
+            updateArrayCopy[productId - 1].inCart = false;
+            setProductsList(updateArrayCopy);
+        }
+        // The reset conditional will only trigger when undefined as passed as productId
+        if (action === "reset" && productId === undefined) {
+            let resetInCart = productsList.map((product) => {
+                product.inCart = false;
+                return product;
+            });
+            setProductsList(resetInCart);
+        }
+    };
 
-  return (
-    <>
-      <ul className="productGrid">
-      <input className="searchbar" type="text" id="searchInput" onChange={SearchSort} placeholder="Zoek een product" />
-        <li className="u-list-style-none productGrid__shoppingCart">
-          <ShoppingCart
-            userId={user}
-            cartItems={cartItems}
-            resetCart={() => updateCart(undefined, "reset")}
-            removeItem={(productId) => updateCart(productId, 'remove')}
-          />
-        </li>
-        {products}
-      </ul>
-    </>
-  );
+    // Mapping of fetched API products to list.
+    const products = productsList.map((product) => {
+        return (
+            <li
+                className="u-list-style-none productItem"
+                key={product.id.toString()}
+                style={{ display: "block" }}
+            >
+                <ProductCard
+                    key={product.key}
+                    product={product}
+                    updateCart={(productId, action) =>
+                        updateCart(productId, action)
+                    }
+                    inCart={product.inCart}
+                    ordered={product.ordered}
+                />
+            </li>
+        );
+    });
+
+    // Maps a copy array of productsList with inCart === true for the <ShoppingCart /> component
+    const cartItems = productsList.filter(function (product) {
+        return product.inCart === true;
+    });
+
+    const SearchSort = () => {
+        var searchInput = document.getElementById("searchInput");
+        var filter = searchInput.value.toUpperCase().split(" ");
+        var i;
+        console.log(filter);
+        for (i = 0; i < products.length; i++) {
+            var currentProduct = products[i].props.children.props.product;
+
+            var prodBrand = currentProduct.brand.toUpperCase();
+            var prodCat = currentProduct.category.toUpperCase();
+            var prodDescription = currentProduct.description.toUpperCase();
+            var prodModel = currentProduct.model.toUpperCase();
+            var prodSubCategory = currentProduct.sub_category.toUpperCase();
+
+            var j;
+            document.getElementsByClassName("productItem")[i].style.display =
+                "none";
+
+            for (j = 0; j < filter.length; j++) {
+                if (
+                    prodBrand.includes(filter[j]) ||
+                    prodCat.includes(filter[j]) ||
+                    prodDescription.includes(filter[j]) ||
+                    prodModel.includes(filter[j]) ||
+                    prodSubCategory.includes(filter[j])
+                ) {
+                    document.getElementsByClassName("productItem")[
+                        i
+                    ].style.display = "block";
+                    var k;
+                    for (k = 0; k < filter.length; k++) {
+                        if (
+                            !(
+                                prodBrand.includes(filter[k]) ||
+                                prodCat.includes(filter[k]) ||
+                                prodDescription.includes(filter[k]) ||
+                                prodModel.includes(filter[k]) ||
+                                prodSubCategory.includes(filter[k])
+                            )
+                        ) {
+                            document.getElementsByClassName("productItem")[
+                                i
+                            ].style.display = "none";
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    return (
+        <>
+            <ul className="productGrid">
+                <input
+                    className="searchbar"
+                    type="text"
+                    id="searchInput"
+                    onChange={SearchSort}
+                    placeholder="Zoek een product"
+                />
+                <li className="u-list-style-none productGrid__shoppingCart">
+                    <ShoppingCart
+                        userId={user}
+                        cartItems={cartItems}
+                        resetCart={() => updateCart(undefined, "reset")}
+                        removeItem={(productId) =>
+                            updateCart(productId, "remove")
+                        }
+                    />
+                </li>
+                {products}
+            </ul>
+        </>
+    );
 };
 
 export default ProductPage;
